8a92f68eea9defa3c73c29d556c691edfe90b524
Chad Thompson <chad_thompson@mac.com>
2015-05-20 11:05:02 -0500

    Verify that the directories are there

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,8 @@
 node /^web/ {
   notify{"This is a web server in the ${::environment} environment": }
 
+  file{"/var/www/": ensure => directory} -> file{"/var/www/html": ensure => directory, }
+
   $web_content = hiera('web_content')
   file{"/var/www/html/index.html":
     ensure => file,
