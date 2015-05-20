851fc98d0d05de8232603b3c9618bf03d7e0500c
Chad Thompson <chad_thompson@mac.com>
2015-05-20 11:00:41 -0500

    Add a site manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,10 @@
 node /^web/ {
   notify{"This is a web server in the ${::environment} environment": }
+
+  $web_content = hiera('web_content')
+  file{"/var/www/html/index.html":
+    ensure => file,
+    content => $web_content,
+  }
+
 }
