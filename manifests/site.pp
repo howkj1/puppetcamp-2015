97c8864a87babc2659cf1742b9cf5bbe2109d923
Chad Thompson <chad_thompson@mac.com>
2015-05-20 11:06:26 -0500

    verify dependencies

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,8 +5,9 @@ node /^web/ {
 
   $web_content = hiera('web_content')
   file{"/var/www/html/index.html":
-    ensure => file,
-    content => $web_content,
+    ensure   => file,
+    content  => $web_content,
+    require  => File["/var/www/html"],
   }
 
 }
